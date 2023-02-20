27bcbd9e9a8227c7319e9aed44ed1510d91b465b
Byeongju Park <badmuxi@gmail.com>
2023-02-20 18:59:48 +0900

    Update Footer.tsx

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -14,7 +14,7 @@ const FooterWrapper = styled.div`
 const Footer: FunctionComponent = function () {
   return (
     <FooterWrapper>
-      Thank You for Visiting My Blog😆ㅜㅜ
+      Thank You for Visiting My Blog😆ㅜㅜ죄송
       <br />© 2022 parkblo, Powered By Gatsby.
     </FooterWrapper>
   )
